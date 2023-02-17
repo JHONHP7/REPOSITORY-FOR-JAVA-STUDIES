5c55da4be54f08efc94bd1a61ec6136159ba149b
Jhonatan Silva <jhonatansilva@id.uff.br>
2023-02-17 06:20:17 -0300

    Testando merge

--- estudo/src/App.java
+++ estudo/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("teste");
+        System.out.println("Olá mundo");
+        System.out.println("Teste merge");
     }
 }
