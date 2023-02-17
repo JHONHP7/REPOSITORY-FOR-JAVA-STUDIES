46accdc704f79a96fdbf36fdf5c1bfce19f78e1a
Jhonatan Silva <jhonatansilva@id.uff.br>
2023-02-17 06:25:26 -0300

    Testando branchs

--- estudo/src/App.java
+++ estudo/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Olá mundo");
         System.out.println("Teste merge");
+        System.out.println("Teste merge 2");
     }
 }
