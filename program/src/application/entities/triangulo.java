d2e1c75ff72f7fb195c16931b35e7d0715497906
Jhonatan Silva <jhonatansilva@id.uff.br>
2023-02-18 02:59:05 -0300

    Update program

--- program/src/application/entities/triangulo.java
+++ program/src/application/entities/triangulo.java
@@ -1,7 +1,12 @@
-package application.entities;
+package application.entities; // pacote classe
 
-public class triangulo {
+public class triangulo { // classe triangulo
     public double a;
     public double b;
     public double c;
+
+    public double area(){ // Função igual a método não precisa de a,b,c pois já está dentro
+        double p = (a + b +c)/2.0;
+        return  Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
 }
